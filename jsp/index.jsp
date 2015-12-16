c5b011a0c17cedee24e54dd40e6b793c61796e0c
nmeln <nazariy.melnychuk9@gmail.com>
2015-12-16 16:55:30 +0200

    Continued

---
+++ jsp/index.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
+<%@ page import="com.company.Field" %>
+<html>
+<head>
+    <title>Success</title>
+</head>
+<body>
+<img src="123.jpg">
+</body>
+</html>
--- src/com/company/AppConfig.java
+++ src/com/company/AppConfig.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @ComponentScan("com.company")
+@EnableWebMvc
 @PropertySource("classpath:/output.properties")
 @PropertySource("classpath:/velocity.properties")
 public class AppConfig {
